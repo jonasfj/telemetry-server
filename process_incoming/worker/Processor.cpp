83cdd1fa8d90e6521347d694c20bd9b246784e4f
Jonas Finnemann Jensen <jopsen@gmail.com>
2013-10-25 14:40:18 -0700

    Added XZ multiplexer

---
+++ process_incoming/worker/Processor.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include <string>
+#include <fstream>
+
+#include "RecordWriter.h"
+
+#define MAX_SIZE    1024 * 1024 * 1024
+
+using namespace std;
+using namespace mozilla::Telemetry;
+
+/** Main function */
+int main(int argc, const char* argv[])
+{
+  string inputFile = "raw-unsorted-data.log";
+  string workingFolder = "working/";
+  string uploadFolder = "upload/";
+
+  RecordWriter writer(workingFolder, uploadFolder, MAX_SIZE, MAX_SIZE);
+
+  ifstream input(inputFile);
+
+  int i = 0;
+
+  // Read input line by line
+  string line;
+  while(getline(input, line)) {
+
+    // Find first space, this is where we split the file
+    size_t split = line.find_first_of(' ');
+    if (split == string::npos) {
+      // No space, split... must be invalid line
+      fprintf(stderr, "Line doesn't contain a space!\n");
+      continue;
+    }
+
+    // Extra path
+    string path = line.substr(0, split);
+  
+    // Find second space
+    split = line.find_first_of(' ', split + 1);
+    const char* buffer = line.c_str() + split + 1;
+
+    writer.Write(path, buffer, strlen(buffer));
+
+    i++;
+  }
+
+  writer.Finalize();
+
+  fprintf(stderr, "Finish writing %i records\n", i);
+  return 0;
+}
+
+
+
---
+++ process_incoming/worker/RecordWriter.h
@@ -0,0 +1,92 @@
+#ifndef RecordWriter_h
+#define RecordWriter_h
+
+#include <string>
+#include <unordered_map>
+
+namespace mozilla {
+
+namespace Telemetry {
+
+/**
+ * RecordWriter is responsible for writing records into temporary files stored
+ * in mWorkFolder, compressing and moving them to mUploadFolder, when the files
+ * grow large enough or RecordWriter::Finalize() is called.
+ * The RecordWrite will attempt to do on-the-fly compression of as many files
+ * especially commonly used files as much as possible. Whilst providing the
+ * desired compression preset and respecting soft memory limits,
+ */
+class RecordWriter
+{
+  class OutputFile;
+  typedef std::unordered_map<std::string, OutputFile*> FileMap;
+public:
+  /**
+   * Create a RecordWriter
+   * @param aWorkFolder           work folder for temporary files
+   * @param aUploadFolder         destination folder for files to be uploaded
+   * @param aMaxUncompressedSize  maximum number of bytes before rotating file
+   * @param aSoftMemoryLimit      a soft limit on memory usage
+   * @param aCompressionPreset    desired compression level ranges from 0 to 9
+   *
+   * Remark: aSoftMemoryLimit will be used to determine the number of files that
+   * can be compressed on-the-fly, it maybe violated.
+   */
+  RecordWriter(const std::string& aWorkFolder, const std::string& aUploadFolder,
+               uint64_t aMaxUncompressedSize, size_t aSoftMemoryLimit,
+               uint32_t aCompressionPreset);
+
+  /** Write record with given filter path */
+  bool Write(const std::string& aPath, const char* aRecord, size_t aLength);
+
+  /** Close all open files, compress and move to upload folder */
+  bool Finalize();
+
+  /** Get absolute path to work folder, ending with a slash */
+  const std::string& WorkFolder() const
+  {
+    return mWorkFolder;
+  }
+
+  /** Get absolute path to upload folder, ending with a slash */
+  const std::string& UploadFolder() const
+  {
+    return mUploadFolder;
+  }
+
+  /** Get compression preset */
+  uint32_t CompressionPreset() const
+  {
+    return mCompressionPreset;
+  }
+
+  /** Get max uncompressed size allowed */
+  uint64_t MaxUncompressedSize() const
+  {
+    return mMaxUncompressedSize;
+  }
+
+  /** Get a new UUID */
+  const std::string& GetUUID()
+  {
+    return mUUIDSource.GetUUID();
+  }
+
+private:
+  std::string mWorkFolder;
+  std::string mUploadFolder;
+  uint64_t    mMaxUncompressedSize;
+  size_t      mSoftMemoryLimit;
+  uint32_t    mCompressionPreset;
+  FileMap     mFileMap;
+  size_t      mRecordsSinceLastReprioritization;
+  Utils::UUIDGenerator mUUIDSource;
+
+  /** Reprioritize on-the-fly compression */
+  bool ReprioritizeCompression();
+};
+
+} // namespace Telemetry
+} // namespace mozilla
+
+#endif // RecordWriter_h
